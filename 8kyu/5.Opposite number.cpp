d3f356e1581aea4a46e1ac98e670e28f58d656a0
xubo@surfilter.com <xubo@surfilter.com>
2021-08-25 16:47:06 +0800

    Create 5.Opposite number.cpp

---
+++ 8kyu/5.Opposite number.cpp
@@ -0,0 +1,16 @@
+//5.Opposite number
+
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+// solution1
+int opposite(int number) 
+{
+	return -1* number;
+}
