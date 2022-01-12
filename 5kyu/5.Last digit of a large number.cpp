122995e7f169bb0c894a154667b3ca18e0c58386
xubo@surfilter.com <my0127912@hotmail.com>
2022-01-12 14:22:32 +0800

    Create 5.Last digit of a large number.cpp

---
+++ 5kyu/5.Last digit of a large number.cpp
@@ -0,0 +1,27 @@
+/*
+5.Last digit of a large number
+
+Instructions
+
+Define a function that takes in two non-negative integers a and b and 
+returns the last decimal digit of a^b . Note that a and b may be very large!
+
+For example, the last decimal digit of 9^7 is 9, since 9^7= 47829699 . 
+The last decimal digit of (2^200)2^300, which has over 10^92 decimal digits, 
+is 6. Also, please take 0^0 to be 1.
+
+You may assume that the input will always be valid.
+
+Remarks
+JavaScript, C++, R, PureScript
+Since these languages don't have native arbitrarily large integers, 
+your arguments are going to be strings representing non-negative integers instead.
+*/
+
+#include <string>
+
+int last_digit(const std::string &str1, const std::string &str2) {
+  return 0;
+}
+
+
